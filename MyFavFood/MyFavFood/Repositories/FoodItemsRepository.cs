773e0d086e72602aa6d53d674c7d93c696d7e5a4
JasmitSingh <jasmitsingh@webuy.com>
2019-01-21 14:56:52 +0530

    Changed the title

--- MyFavFood/MyFavFood/Repositories/FoodItemsRepository.cs
+++ MyFavFood/MyFavFood/Repositories/FoodItemsRepository.cs
@@ -24,7 +24,17 @@ namespace MyFavFood.Repositories
                 new FoodItem { ItemId = 7, ItemName = "Crabs Chilli" },
                 new FoodItem { ItemId = 8, ItemName = "Laksa" },
                 new FoodItem { ItemId = 9, ItemName = "Curry Fish Head" },
-                new FoodItem { ItemId = 10, ItemName = "Crabs Pepper" }
+                new FoodItem { ItemId = 10, ItemName = "Crabs Pepper" },
+                new FoodItem { ItemId = 11, ItemName = "Murg Makhani" },
+                new FoodItem { ItemId = 12, ItemName = "Tandoori Chicken" },
+                new FoodItem { ItemId = 13, ItemName = "Chicken Tikka Masala" },
+                new FoodItem { ItemId = 14, ItemName = "Rogan Josh" },
+                new FoodItem { ItemId = 15, ItemName = "Malai Kofta" },
+                new FoodItem { ItemId = 16, ItemName = "Chole" },
+                new FoodItem { ItemId = 17, ItemName = "Palak Paneer" },
+                new FoodItem { ItemId = 18, ItemName = "Kaali Daal" },
+                new FoodItem { ItemId = 19, ItemName = "Matar Paneer" },
+                new FoodItem { ItemId = 20, ItemName = "Samosas" }
             };
         }
     }
